d5b601447291507a912fcbd6543fc97e6b93411c
unknown <emilio.rodriguez@correo.ucu.edu.uy>
2024-09-26 17:21:12 -0300

    Agregado plugins y servicios, primera parte de la bd completa

---
+++ backend/services/database.ts
@@ -0,0 +1,12 @@
+import pg from 'pg'
+
+ 
+const pool = new pg.Pool();
+ 
+export const query = async (text:string, params?: (string | number | string[] | number[]) [] ) => {
+  // const start = Date.now()
+  const res = await pool.query(text, params)
+  // const duration = Date.now() - starts
+  // console.log('executed query', { text, duration, rows: res.rowCount })
+  return res
+}
