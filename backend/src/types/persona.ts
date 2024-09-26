b903a79e35d806d9ce42b9a4d3b1450b736bc63a
FranciscoLima18 <francisco.lima@correo.ucu.edu.uy>
2024-09-26 18:38:45 -0300

    dependencias de typebox faltantes y descomentado types.

--- backend/src/types/persona.ts
+++ backend/src/types/persona.ts
@@ -1,18 +1,18 @@
-// import { Static, Type } from '@sinclair/typebox';
+ import { Static, Type } from '@sinclair/typebox';
 
-// const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&_-])[A-Za-z\d!@#$%^&_-]{8,20}$/;
-// const cedulaRegex = /^\d{1,3}\.\d{3}\.\d{3}-\d$/;
+ const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&_-])[A-Za-z\d!@#$%^&_-]{8,20}$/;
+ const cedulaRegex = /^\d{1,3}\.\d{3}\.\d{3}-\d$/;
 
 
-// export const PersonaPostSchema = Type.Object(
-//     {
-//         email: Type.String({ format: "email" }),
-//         contraseña: Type.String({ minLength: 5, maxLength: 20, pattern: passwordRegex.source }),
-//         repetirContraseña: Type.String({ minLength: 5, maxLength: 20, pattern: passwordRegex.source }),
-//         cedula: Type.String({ pattern: cedulaRegex.source }),
-//         foto: Type.Object({})
-//     },
-//     { additionalProperties: false }
-// )
+ export const PersonaPostSchema = Type.Object(
+     {
+         email: Type.String({ format: "email" }),
+         contraseña: Type.String({ minLength: 5, maxLength: 20, pattern: passwordRegex.source }),
+         repetirContraseña: Type.String({ minLength: 5, maxLength: 20, pattern: passwordRegex.source }),
+         cedula: Type.String({ pattern: cedulaRegex.source }),
+         foto: Type.Object({})
+     },
+     { additionalProperties: false }
+ )
 
 // export type PersonaPostSchema = Static<typeof PersonaPostSchema>
