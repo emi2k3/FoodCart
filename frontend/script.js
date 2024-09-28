ad016274ca7fb524d57c02c7f669da4692cfca1f
FranciscoLima18 <francisco.lima@correo.ucu.edu.uy>
2024-09-28 18:37:17 -0300

    ahora el login je

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 document.getElementById('loginBtn').addEventListener('click', function() {
     alert('Redirigiendo a la página de inicio de sesión...');
-    window.location.href = './Login/form.html';
+    window.location.href = './Login/index.html';
 });
 
 document.getElementById('registerBtn').addEventListener('click', function() {
