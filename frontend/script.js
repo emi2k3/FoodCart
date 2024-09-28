669b3a0691817bd105b66e28047a3866c88899f3
FranciscoLima18 <francisco.lima@correo.ucu.edu.uy>
2024-09-28 18:34:35 -0300

    Cambio del href bien redirigido.

--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,9 @@
 document.getElementById('loginBtn').addEventListener('click', function() {
     alert('Redirigiendo a la página de inicio de sesión...');
-    window.location.href = '../login/login.html'
+    window.location.href = './Login/form.html';
 });
 
 document.getElementById('registerBtn').addEventListener('click', function() {
     alert('Redirigiendo a la página de registro...');
-    window.location.href = '../register/register.html'
-});
+    window.location.href = './Register/form.html';
+}); 
