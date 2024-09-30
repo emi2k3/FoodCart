6929947ab0db22055058fd9f2b5c59f294ed4a1c
unknown <emilio.rodriguez@correo.ucu.edu.uy>
2024-09-29 22:15:03 -0300

    cambios

--- backend/src/routes/usuarios/index.ts
+++ backend/src/routes/usuarios/index.ts
@@ -31,57 +31,51 @@ const usuarioRoute: FastifyPluginAsync = async (fastify, opts): Promise<void> =>
       if (postUsuario.foto && Object.keys(postUsuario.foto).length > 0) {
         try {
           const fileBuffer = (postUsuario.foto as Buffer);
-          const fileName = join(process.cwd(), "Resources", postUsuario.email + ".jpg")
+          const fileName = join(process.cwd(), "Resources", postUsuario.email + ".jpg");
           writeFileSync(fileName, fileBuffer);
         } catch (error) {
           console.error("Error al intentar crear la imagen:", error);
-          return reply.status(500).send("Hubo un error al intentar crear la imagen")
+          return reply.status(500).send("Hubo un error al intentar crear la imagen");
         }
       }
-
 
       try {
         if (postUsuario.apto != null || postUsuario.apto != undefined) {
           direccionId = await query("INSERT INTO direccion (numero, calle, apto) VALUES ($1, $2, $3) RETURNING id",
-            [postUsuario.numero, postUsuario.calle, postUsuario.apto])
+            [postUsuario.numero, postUsuario.calle, postUsuario.apto]);
         } else {
           direccionId = await query("INSERT INTO direccion (numero, calle) VALUES ($1, $2) RETURNING id",
-            [postUsuario.numero, postUsuario.calle])
+            [postUsuario.numero, postUsuario.calle]);
         }
-
       } catch (error) {
         console.error("Error al intentar crear la dirección:", error);
-        return reply.status(500).send("Hubo un error al intentar crear la dirección")
+        return reply.status(500).send("Hubo un error al intentar crear la dirección");
       }
 
       try {
         telefonoId = await query("INSERT INTO telefono (numeroTel) VALUES ($1) RETURNING id",
-          [postUsuario.telefono])
-      }
-      catch (error) {
-        console.error("Error al intentar crear el telefono:", error);
-        return reply.status(500).send("Hubo un error al intentar crear el telefono")
+          [postUsuario.telefono]);
+      } catch (error) {
+        console.error("Error al intentar crear el teléfono:", error);
+        return reply.status(500).send("Hubo un error al intentar crear el teléfono");
       }
 
       try {
         usuarioId = await query(`INSERT INTO usuario(nombre, apellido, email, contraseña, id_direccion, id_telefono) 
-          VALUES ($1,$2, $3, crypt($4, gen_salt('bf')), $5, $6) RETURNING id`,
-          [postUsuario.nombre, postUsuario.apellido, postUsuario.email, postUsuario.contraseña, direccionId.rows[0].id, telefonoId.rows[0].id])
+          VALUES ($1, $2, $3, crypt($4, gen_salt('bf')), $5, $6) RETURNING id`,
+          [postUsuario.nombre, postUsuario.apellido, postUsuario.email, postUsuario.contraseña, direccionId.rows[0].id, telefonoId.rows[0].id]);
 
-        await query("UPDATE direccion SET id_usuario = (SELECT id FROM usuario WHERE email = $1) WHERE id = $2",
-          [postUsuario.email, direccionId.rows[0].id]);
+        await query("UPDATE direccion SET id_usuario = $1 WHERE id = $2", 
+          [usuarioId.rows[0].id, direccionId.rows[0].id]);
 
-        await query("UPDATE telefono SET id_usuario = (SELECT id FROM usuario WHERE email = $1) WHERE id = $2",
-          [postUsuario.email, telefonoId.rows[0].id]);
+        await query("UPDATE telefono SET id_usuario = $1 WHERE id = $2",
+          [usuarioId.rows[0].id, telefonoId.rows[0].id]);
 
-        await query(`INSERT INTO usuarios_direcciones(usuario_id, direccion_id) 
-            VALUES ($1,$2)`,
-          [usuarioId.rows[0].id, direccionId.rows[0].id])
-
+        await query(`INSERT INTO usuarios_direcciones(usuario_id, direccion_id) VALUES ($1, $2)`,
+          [usuarioId.rows[0].id, direccionId.rows[0].id]);
 
         return reply.status(201).send("Se creó correctamente el usuario");
-      }
-      catch (error) {
+      } catch (error) {
         console.error("Error al intentar crear al usuario:", error);
         return reply.status(500).send("Hubo un error al intentar crear al usuario.");
       }
@@ -143,52 +137,45 @@ const usuarioRoute: FastifyPluginAsync = async (fastify, opts): Promise<void> =>
       try {
         if (postUsuario.foto != null || postUsuario.foto != undefined) {
           const fileBuffer = (postUsuario.foto as Buffer);
-          const fileName = join(process.cwd(), "Resources", postUsuario.email + ".jpg")
+          const fileName = join(process.cwd(), "Resources", postUsuario.email + ".jpg");
           writeFileSync(fileName, fileBuffer);
         }
-      }
-      catch (error) {
+      } catch (error) {
         console.error("Error al intentar crear la imagen:", error);
-        return reply.status(500).send("Hubo un error al intentar crear la imagen")
+        return reply.status(500).send("Hubo un error al intentar crear la imagen");
       }
 
       if (postUsuario.apto != null || postUsuario.apto != undefined) {
         try {
           await query("UPDATE direccion set numero = $1 , calle = $2 , apto = $3 WHERE id_usuario = $4",
-            [postUsuario.numero, postUsuario.calle, postUsuario.apto, idt])
-
-        }
-        catch {
+            [postUsuario.numero, postUsuario.calle, postUsuario.apto, idt]);
+        } catch {
           return reply.status(500).send("Hubo un error al intentar actualizar la dirección.");
         }
-      }
-      else {
+      } else {
         try {
-          await query("UPDATE direccion set numero = $1 , calle = $2  WHERE id_usuario = $4",
-            [postUsuario.numero, postUsuario.calle, idt])
-
-        }
-        catch {
+          await query("UPDATE direccion set numero = $1 , calle = $2 WHERE id_usuario = $4",
+            [postUsuario.numero, postUsuario.calle, idt]);
+        } catch {
           return reply.status(500).send("Hubo un error al intentar actualizar la dirección.");
         }
       }
+
       try {
-        await query("UPDATE telefono set numeroTel = $1 where id_usuario = $2",
-          [postUsuario.telefono, idt])
+        await query("UPDATE telefono set numeroTel = $1 WHERE id_usuario = $2",
+          [postUsuario.telefono, idt]);
       } catch (error) {
-        return reply.status(500).send("Hubo un error al intentar actualizar el telefono.");
+        return reply.status(500).send("Hubo un error al intentar actualizar el teléfono.");
       }
 
       try {
-        await query("UPDATE personas set nombre = $1, email = $2, contraseña = crypt($3, gen_salt('bf')) where id = $4)",
-          [postUsuario.nombre, postUsuario.email, postUsuario.contraseña, idt])
-        return reply.status(201).send("El usuario se editó correctamente")
+        await query("UPDATE personas set nombre = $1, email = $2, contraseña = crypt($3, gen_salt('bf')) WHERE id = $4",
+          [postUsuario.nombre, postUsuario.email, postUsuario.contraseña, idt]);
+        return reply.status(201).send("El usuario se editó correctamente");
       } catch (error) {
-        return reply
-          .status(500)
-          .send("Hubo un error al intentar actualizar al usuario.");
+        return reply.status(500).send("Hubo un error al intentar actualizar al usuario.");
       }
-    },
+    }
   });
 
   fastify.delete("/:id", {
@@ -228,14 +215,11 @@ const usuarioRoute: FastifyPluginAsync = async (fastify, opts): Promise<void> =>
       try {
         await query("DELETE FROM telefono WHERE id_usuario = $1", [id]);
       } catch (error) {
-        return reply.status(500).send("Hubo un error al intentar borrar el telefono.");
+        return reply.status(500).send("Hubo un error al intentar borrar el teléfono.");
       }
       return reply.status(204).send();
-    },
-        return reply.status(500).send("Hubo un error al intentar actualizar al usuario.");
-      }
     }
-
   });
 };
+
 export default usuarioRoute;
