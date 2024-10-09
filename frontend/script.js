cbc080ed01ad6f4571c7e6eb28b19ba2aa0a4622
unknown <emilio.rodriguez@correo.ucu.edu.uy>
2024-10-09 14:37:02 -0300

    Angular

--- frontend/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('loginBtn').addEventListener('click', function() {
-    alert('Redirigiendo a la página de inicio de sesión...');
-    window.location.href = './Login/index.html';
-});
-
-document.getElementById('registerBtn').addEventListener('click', function() {
-    alert('Redirigiendo a la página de registro...');
-    window.location.href = './Register/form.html';
-}); 
