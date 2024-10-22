8399f6fa11a7af15a36738790a3c026eb7ab528e
GabiFares <computadorafares18@gmail.com>
2024-10-22 01:57:03 -0300

    cambios menos los de local, proximamente a subir

--- backend/src/routes/local/index.ts
+++ backend/src/routes/local/index.ts
@@ -1,62 +1,69 @@
 import { FastifyPluginAsync } from "fastify";
 
+const localesRoute: FastifyPluginAsync = async (
+  fastify,
+  opts
+): Promise<void> => {
+  // ################################################### GET ###################################################
 
-const localesRoute: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-    fastify.get('/', {
-        schema: {
-          description: ' ',
-          tags: ['Locales'],
-          security: [{ BearerAuth: [] }],
-          response: {
-            200: {}
-          },
-        },
-        onRequest: [fastify.authenticate],
-        handler: async function (request, reply) {
+  fastify.get("/", {
+    schema: {
+      summary: "Listado de todos los locales",
+      description: " ",
+      tags: ["Locales"],
+      security: [{ BearerAuth: [] }],
+      response: {
+        200: {},
+      },
+    },
+    onRequest: [fastify.authenticate],
+    handler: async function (request, reply) {},
+  });
 
-        }
-      });
-      fastify.put('/:id', {
-        schema: {
-          description: ' ',
-          tags: ['Locales'],
-          security: [{ BearerAuth: [] }],
-          response: {
-            200: {}
-          },
-        },
-        onRequest: [fastify.authenticate],
-        handler: async function (request, reply) {
+  // ################################################### PUT ###################################################
 
-        }
-      });
-      fastify.delete('/:id', {
-        schema: {
-          description: ' ',
-          tags: ['Locales'],
-          security: [{ BearerAuth: [] }],
-          response: {
-            200: {}
-          },
-        },
-        onRequest: [fastify.authenticate],
-        handler: async function (request, reply) {
+  fastify.put("/:id", {
+    schema: {
+      summary: "Modificación de un local por su id",
+      description: " ",
+      tags: ["Locales"],
+      security: [{ BearerAuth: [] }],
+      response: {
+        200: {},
+      },
+    },
+    onRequest: [fastify.authenticate],
+    handler: async function (request, reply) {},
+  });
 
-        }
-      });
-      fastify.post('/', {
-        schema: {
-          description: ' ',
-          tags: ['Locales'],
-          security: [{ BearerAuth: [] }],
-          response: {
-            200: {}
-          },
-        },
-        onRequest: [fastify.authenticate],
-        handler: async function (request, reply) {
+  // ################################################### DELETE ##################################################
 
-        }
-      });
-}
+  fastify.delete("/:id", {
+    schema: {
+      description: " ",
+      tags: ["Locales"],
+      security: [{ BearerAuth: [] }],
+      response: {
+        200: {},
+      },
+    },
+    onRequest: [fastify.authenticate],
+    handler: async function (request, reply) {},
+  });
+
+  // ################################################### POST ###################################################
+
+  fastify.post("/", {
+    schema: {
+      description: " ",
+      tags: ["Locales"],
+      security: [{ BearerAuth: [] }],
+      response: {
+        200: {},
+      },
+    },
+    onRequest: [fastify.authenticate],
+    handler: async function (request, reply) {},
+  });
+};
 export default localesRoute;
